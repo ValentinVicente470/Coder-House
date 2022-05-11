7d47491633bd5e9af8671725bd0db592fdcbf32e
ValentinVicente470 <104231755+ValentinVicente470@users.noreply.github.com>
2022-05-11 18:35:20 -0300

    Add files via upload

---
+++ server HANDLEBARS/server.js
@@ -0,0 +1,48 @@
+const express = require ('express')
+const Contenedor = require ('./contenedor')
+const handlebars = require('express-handlebars')
+const { Router } = express
+const app = express()
+
+app.engine ('handlebars', handlebars.engine())
+app.set ('views', './views')
+app.set ('view engine', 'handlebars')
+
+app.use ( express.static ('public') )
+app.use ( express.urlencoded( { extended: true } ) )
+app.use ( express.json() )
+
+//router
+const routerProductos = new Router() 
+
+
+//endpoints
+routerProductos.post ('/post', async (req, res) => {
+    const obj = (req.body)
+    await Contenedor.save(obj)
+    res.redirect ('/api/productos')
+})
+
+routerProductos.get ('/lista', async (req, res) => {
+    const objetos = await Contenedor.getall()
+    res.render ( 'lista-productos', {objetos} )
+})
+
+routerProductos.get('/', async (req, res) => {
+    const objeto = await Contenedor.getall()
+    res.render('agregar-prods', { objeto })
+})
+
+
+//carga de routers
+app.use ('/api/productos', routerProductos)
+
+
+//server
+const PORT = 8082
+
+const server = app.listen(PORT, () => {
+    console.log ('server HTTP escuchando en el puerto' + PORT)
+})
+server.on ('error', error => console.log (`error en el server ${error}`))
+
